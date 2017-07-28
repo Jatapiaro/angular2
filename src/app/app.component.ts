5141e92738b5ccc374abaa50e91a35b1e64cf71a
Jacobo <jacob.et.cetera@gmail.com>
2017-07-27 23:51:01 -0500

    Just an example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+  nombre:string = 'Jacobo';
+  apellido:string = 'Tapia';
+  edad:number = 50;
 }
